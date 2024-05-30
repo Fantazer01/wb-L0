7c70519f852a16debcb63acbc0105e59418962ec
Oleg <olegkrygin9@gmail.com>
2024-05-31 01:17:59 +0300

    добавил тэги для правильного отбражения полей json

--- order.go
+++ order.go
@@ -3,55 +3,55 @@ package main
 import "time"
 
 type Delivery struct {
-	Name    string
-	Phone   string
-	Zip     string
-	City    string
-	Address string
-	Region  string
-	Email   string
+	Name    string `json:"name"`
+	Phone   string `json:"phone"`
+	Zip     string `json:"zip"`
+	City    string `json:"city"`
+	Address string `json:"address"`
+	Region  string `json:"region"`
+	Email   string `json:"email"`
 }
 
 type Payment struct {
-	Transaction   string
-	Request_id    string
-	Currency      string
-	Provider      string
-	Amount        int
-	Payment_dt    int
-	Bank          string
-	Delivery_cost int
-	Goods_total   int
-	Custom_fee    int
+	Transaction   string `json:"transaction"`
+	Request_id    string `json:"request_id"`
+	Currency      string `json:"currency"`
+	Provider      string `json:"provider"`
+	Amount        int    `json:"amount"`
+	Payment_dt    int    `json:"payment_dt"`
+	Bank          string `json:"bank"`
+	Delivery_cost int    `json:"delivery_cost"`
+	Goods_total   int    `json:"goods_total"`
+	Custom_fee    int    `json:"custom_fee"`
 }
 
 type Item struct {
-	Chrt_id      int
-	Track_number string
-	Price        int
-	Rid          string
-	Name         string
-	Sale         int
-	Size         string
-	Total_price  int
-	Nm_id        int
-	Brand        string
-	Status       int
+	Chrt_id      int    `json:"chrt_id"`
+	Track_number string `json:"track_number"`
+	Price        int    `json:"price"`
+	Rid          string `json:"rid"`
+	Name         string `json:"name"`
+	Sale         int    `json:"sale"`
+	Size         string `json:"size"`
+	Total_price  int    `json:"total_price"`
+	Nm_id        int    `json:"nm_id"`
+	Brand        string `json:"brand"`
+	Status       int    `json:"status"`
 }
 
 type Order struct {
-	Order_uid          string
-	Track_number       string
-	Entry              string
-	Delivery           Delivery
-	Payment            Payment
-	Items              []Item
-	Locale             string
-	Internal_signature string
-	Customer_id        string
-	Delivery_service   string
-	Shardkey           string
-	Sm_id              int
-	Date_created       time.Time
-	Oof_shard          string
+	Order_uid          string    `json:"order_uid"`
+	Track_number       string    `json:"track_number"`
+	Entry              string    `json:"entry"`
+	Delivery           Delivery  `json:"delivery"`
+	Payment            Payment   `json:"payment"`
+	Items              []Item    `json:"items"`
+	Locale             string    `json:"locale"`
+	Internal_signature string    `json:"internal_signature"`
+	Customer_id        string    `json:"customer_id"`
+	Delivery_service   string    `json:"delivery_service"`
+	Shardkey           string    `json:"shardkey"`
+	Sm_id              int       `json:"sm_id"`
+	Date_created       time.Time `json:"date_created"`
+	Oof_shard          string    `json:"oof_shard"`
 }
